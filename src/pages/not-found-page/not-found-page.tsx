6079061d9fd38c959eb7ab66bbe9b1584155a18d
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-07-14 00:37:03 +0300

    дописал стили для страницы 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,13 +1,12 @@
 import {Link} from 'react-router-dom';
 import Logo from '../../components/logo/logo';
 import {Helmet} from 'react-helmet-async';
+import { AppRoute } from '../../const';
+import styles from './not-found-page.module.css';
 
 function NotFoundPage(): JSX.Element {
   return (
     <>
-      <Helmet>
-        <title>6 cities: 404</title>
-      </Helmet>
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
@@ -17,12 +16,17 @@ function NotFoundPage(): JSX.Element {
           </div>
         </div>
       </header>
-      <div className="page">
-        <h1>Error 404.
+      <div className={`page ${styles.notFoundPage}`}>
+        <Helmet>
+          <title>6 cities: 404</title>
+        </Helmet>
+        <h1 className={styles.title}>Error 404
           <br />
           <small>Page not found</small>
         </h1>
-        <Link to="/">Go to main page</Link>
+        <p className={styles.text}>
+          <Link to={AppRoute.Main} className={styles.link}>Go to main page</Link>
+        </p>
       </div>
     </>
   );
