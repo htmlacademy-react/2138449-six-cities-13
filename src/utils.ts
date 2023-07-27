800140c816931095ed7448fe0e5681a1d7fea24e
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-07-28 00:16:13 +0300

    5.19. Больше подробностей (часть 2)

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+const DATE_TIME_SEPARATOR = 'T';
+const DATE_TIME_INDEX = 0;
+
+const getFormatDate = (date: string): string => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const currentDate = new Date(date);
+  const currentDay = currentDate.getDate();
+  const currentMonth = months[currentDate.getMonth()];
+
+  return `${currentDay < 10 ? '0' : ''}${currentDay} ${currentMonth}`;
+};
+
+const getDateTime = (date: string): string => date.split(DATE_TIME_SEPARATOR)[DATE_TIME_INDEX];
+
+export { getFormatDate, getDateTime };
