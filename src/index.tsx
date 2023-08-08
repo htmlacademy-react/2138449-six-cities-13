0b36d130e5c685cbde0dfeace329218573435edc
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-08-08 14:16:58 +0300

    вернул моки комментариев

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
-//import { reviewsMock } from './mocks/reviews-mock';
+import { reviewsMock } from './mocks/reviews-mock';
 import { store } from './store';
 import { fetchOffersAction } from './store/api-action';
 
