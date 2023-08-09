c9ca1b8b74f30df0db6adc090cd3c4ad4b0d8643
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-08-09 17:14:43 +0300

    создал header

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -1,7 +1,7 @@
-import Logo from '../../components/logo/logo';
-import {Helmet} from 'react-helmet-async';
+import Header from '../../components/header/header';
 import OffersList from '../../components/offers-list/offers-list';
 import { Offer } from '../../types/offers';
+import {Helmet} from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 type FavoritesProps = {
@@ -17,36 +17,7 @@ function FavoritesPage({offers}: FavoritesProps): JSX.Element {
         <title>6 cities: favorites</title>
       </Helmet>
       <div className="page">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <div className="header__left">
-                <Logo />
-              </div>
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a
-                      className="header__nav-link header__nav-link--profile"
-                      href="#"
-                    >
-                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                      <span className="header__user-name user__name">
-                        Oliver.conner@gmail.com
-                      </span>
-                      <span className="header__favorite-count">3</span>
-                    </a>
-                  </li>
-                  <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
-                      <span className="header__signout">Sign out</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </header>
+        <Header />
         <main className="page__main page__main--favorites">
           <div className="page__favorites-container container">
             <section className="favorites">
